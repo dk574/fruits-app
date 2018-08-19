77878304b6425aafc4a18e39390097ce372f45d1
Dixie Korley <dixiekorley@gmail.com>
2018-08-19 16:36:22 -0400

    setting up the API key from openweather

---
+++ index.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+let API_KEY = '8504ce16be3bdbed41c506c9f9ec7324';
+let city = 'accra';
+let URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+
+request(URL, function(error, response, body) {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(`Body: ${body}`);
+    }
+    return response;
+});
